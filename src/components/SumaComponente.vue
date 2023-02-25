e05ea553dee9c5fad1731078ab43c3a5dd1d15e3
Misael Baez <Misael Baez>
2023-02-24 21:17:47 -0300

    Inicio del proyecto

---
+++ src/components/SumaComponente.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Numero1 {{numero1}}</h1>
+        <input type="number" v-model="numero1">
+        <input type="number" v-model="numero2">
+
+        <button @click="resultado()"> mostrar numero1</button>
+    </div>
+</template>
+
+<script>
+    export default {
+ 
+  data(){
+    return{
+        numero1: 0,
+        numero2: 0,
+        total: 0
+        
+    }
+    
+}, 
+methods:{
+    resultado(){
+        this.total = Number(this.numero1) + Number(this.numero2)
+        console.log('total::: ', this.total);
+      //this.total = parseInt(this.numero1) + parseInt(this.numero2)
+     // this.total = (+ this.numero1) + ( + this.numero2)
+      
+      console.log( typeof this.total);
+      console.log(  this.total);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+
+<h1>{{ titulo }}</h1>
+<br>
+<SumaComponente/>
+    
+  </div>
+</template>
+
+<script>
+import SumaComponente from '@/components/SumaComponente.vue';
+
+export default {
+  name: 'Holaiew',
+  components: {SumaComponente},
+  data(){
+    return{
+
+      titulo:'hola este es el Titulo'
+    }
+
+  }
+}
+</script>
